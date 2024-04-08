e6730de6dd5dfabca143114edb459280eb5ba47a
ichao15 <1214392687@qq.com>
2024-04-08 14:14:13 +0800

    通用工程

---
+++ tj-common/src/main/java/com/tianji/common/autoconfigure/mybatis/MybatisConfig.java
@@ -0,0 +1,48 @@
+package com.tianji.common.autoconfigure.mybatis;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass({MybatisPlusInterceptor.class, BaseMapper.class})
+public class MybatisConfig {
+
+    /**
+     * @deprecated 存在任务更新数据导致updater写入0或null的问题，暂时废弃
+     * @see MyBatisAutoFillInterceptor 通过自定义拦截器来实现自动注入creater和updater
+     */
+    // @Bean
+    // @ConditionalOnMissingBean
+    public BaseMetaObjectHandler baseMetaObjectHandler(){
+        return new BaseMetaObjectHandler();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(@Autowired(required = false)DynamicTableNameInnerInterceptor innerInterceptor) {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        //1.动态表名插件
+        if (innerInterceptor != null) {
+            interceptor.addInnerInterceptor(innerInterceptor);
+        }
+
+        //2.分页插件
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        paginationInnerInterceptor.setMaxLimit(200L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+        //3.自动填充插件
+        interceptor.addInnerInterceptor(new MyBatisAutoFillInterceptor());
+        return interceptor;
+    }
+}
